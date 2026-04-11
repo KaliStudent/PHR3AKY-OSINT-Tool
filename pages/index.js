44ccefd34d2d96b1e956a38a050914985e828155
Br-iON <26982008+KaliStudent@users.noreply.github.com>
2026-04-10 19:28:52 -0500

    Add Home component to display API status

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: 20 }}>
+      <h1>Phone Intelligence API</h1>
+      <p>API is running.</p>
+    </div>
+  );
+}
