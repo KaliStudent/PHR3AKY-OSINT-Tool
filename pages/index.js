f6b8d26d3fd8ba7d979e022679624d27c5a17167
Br-iON <26982008+KaliStudent@users.noreply.github.com>
2026-04-11 04:27:55 -0500

    Rename title from 'Phone Intelligence API' to 'PHR3AKY OSINT Tool'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div style={{ fontFamily: "sans-serif", padding: 20 }}>
-      <h1>Phone Intelligence API</h1>
+    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>PHR3AKY OSINT Tool</h1>
       <p>API is running.</p>
     </div>
   );
